fdf3e3a59ffb02c8f0e50a7c6604a8261afc3446
AnDev <andersosanchez_911@hotmail.com>
2025-10-20 19:46:12 -0400

    Actualizar APP_URL a localhost y eliminar función de paginación en mainModel

--- config/APP.php
+++ config/APP.php
@@ -3,19 +3,17 @@
 	/*----------  
 	Ruta o dominio del servidor
 	----------*/
-	const APP_URL="http://bootcamp-main.loc/";
+	const APP_URL="http://localhost/bootcamp2/";
 
 	/*----------  
 	Nombre de la empresa o compañia
 	----------*/
 	const APP_NAME="BOOTCAMP2";
 
-
 	/*----------  Idioma - Language
 	Español -> es 
 	----------*/
 	const LANG="es";
-
 	/*----------  
 	Datos del correo empresa o compañia
 	----------*/
@@ -26,10 +24,8 @@
 	const APP_COUNTRY="";
 	const APP_ADDRESS="";
 
-
 	/*----------  Marcador de campos obligatorios  ----------*/
 	const APP_FIELD_OBLIGATORY='&nbsp;  &nbsp;';
-
 
 	/*----------  Zona horaria  ----------*/
 	date_default_timezone_set("America/Caracas");
